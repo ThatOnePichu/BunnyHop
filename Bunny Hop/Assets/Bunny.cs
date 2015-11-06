6de33fc85166626a7582ee08b1d48b147b0d782e
ThatOnePichu <bryanraganot@gmail.com>
2015-11-05 18:19:52 -0800

    DIS IS THE REAL ONE
    Bunny Hop 2D

---
+++ Bunny Hop/Assets/Bunny.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bunny : MonoBehaviour {
+
+	public float runSpeed;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			body.velocity = new Vector2(-runSpeed, body.velocity.y);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			body.velocity = new Vector2(runSpeed, body.velocity.y);
+		}
+	}
+}
